c0c4e873160d9ecd53bf6db1604a9583157747b4
vijay singh <vjmail17@gmail.com>
2017-12-17 16:18:37 +0530

    …

---
+++ app/Http/Admin/DashboardController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+
+use Session;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Redirect;
+use DB;
+
+
+class DashboardController extends Controller
+{
+    //
+    public function index()
+       {
+    	    return view('admin.pages.dashboard.home');
+       }
+
+    public function database()
+      {
+        echo "sdf";
+      }
+      
+    public function logout()
+       {
+    	    Session::flush();
+          return redirect('/');
+       }
+}
+
