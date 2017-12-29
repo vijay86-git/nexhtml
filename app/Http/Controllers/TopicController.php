f19e7c2d582fc22344bf494d29691789a90f547e
vijay singh <vjmail17@gmail.com>
2017-12-29 06:26:23 +0530

    …

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -25,9 +25,8 @@ class TopicController extends Controller
     public function index2($slug)
       {
       	  $info = DB::table('topics')->where('slug', $slug)->first();
-
-
       	  $topics = DB::table('topics')->select('id', 'topic', 'slug')->get();
+          $section = DB::table('section')->select('id', 'section')->get();
           return view('front.pages.subject.index',compact('topics', 'info'));
       }
  }
