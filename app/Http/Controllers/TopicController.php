dce07dede0a7463fc20d05e83befd6c34130919b
vijay singh <vjmail17@gmail.com>
2017-12-30 22:58:19 +0530

    ..

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -38,7 +38,7 @@ class TopicController extends Controller
       {
           $subjects =  $this->_subjects;
 
-          $info = Subject::select('id','page_title','meta_keywords','meta_description')->where('slug', $slug)->firstOrFail();
+          $info = Subject::select('id','page_title','meta_keywords','meta_description', 'about as detail')->where('slug', $slug)->firstOrFail();
 
           $section = DB::table('section')->select('id', 'section')->where('subject_id', $info->id)->orderBy('sort', 'asc')->get();
 
