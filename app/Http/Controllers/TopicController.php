244a3f498e62a35ce944df50a941be9e5ca15874
vijay singh <vjmail17@gmail.com>
2017-12-30 22:41:34 +0530

    …

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -38,7 +38,7 @@ class TopicController extends Controller
       {
           $subjects =  $this->_subjects;
 
-          $data     = Subject::select('id', 'name', 'page_title', 'meta_keywords', 'meta_description', 'about as detail')->where('slug', $slug)->firstOrFail();
+          $data     = Subject::select('id', 'name', 'page_title', 'meta_keywords', 'meta_description', 'about as detail')->where('slug', $slug)->firstOrFail()->get();
 
           $section = DB::table('section')->select('id', 'section')->where('subject_id', $data->id)->orderBy('sort', 'asc')->get();
 
