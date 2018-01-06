9d15a49b569f930f293e0693b67b2fafd7be9144
vijay singh <vjmail17@gmail.com>
2018-01-06 07:33:02 +0530

    …

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="msvalidate.01" content="F13063B8299624F18B7C4E98367AE3E1" />
 
         <title>Nexladder</title>
 
