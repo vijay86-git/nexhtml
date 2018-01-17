f3b8a4b4671f6c73396ca1c2f919021a82f7765b
vijay singh <vjmail17@gmail.com>
2018-01-17 07:07:12 +0530

    …

--- resources/views/front/pages/subject/index.blade.php
+++ resources/views/front/pages/subject/index.blade.php
@@ -1,5 +1,33 @@
 @extends('front.layout.template')
 
+@section('nav')
+<nav class="navbar navbar-inverse">
+                  <div class="container-fluid">
+                  <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navBar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span> 
+                    </button>
+                    <a class="navbar-brand" href="#"></a>
+                  </div>
+
+                  <div class="collapse navbar-collapse" id="navBar">
+
+                    <ul class="nav navbar-nav">
+            @foreach($subjects as $sub_data)
+             <li><a style="" href="{{ route('topic', $sub_data->slug) }}">{{ $sub_data->name }}</a></li>
+            @endforeach
+            </ul>
+
+                  </div>
+
+
+                  </div>
+                </nav>
+@stop
+
+
 
 
 {{--
