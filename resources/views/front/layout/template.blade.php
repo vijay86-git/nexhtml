def30358172177e791c3ae82bf9b2fa9ed33b57d
vijay singh <vjmail17@gmail.com>
2018-01-19 06:11:03 +0530

    …

--- resources/views/front/layout/template.blade.php
+++ resources/views/front/layout/template.blade.php
@@ -8,7 +8,7 @@
     <title>Bootstrap 101 Template</title>
     <link rel="stylesheet" href="{{ Helper::loadAssetsFiles('build/assets/css/bootstrap.min.css?v=1') }}">
     <link rel="stylesheet" href="{{ Helper::loadAssetsFiles('build/assets/css/font-awesome.css?v=1') }}">
-    <link rel="stylesheet" href="{{ Helper::loadAssetsFiles('build/assets/css/style.css?v=1') }}">
+    <link rel="stylesheet" href="{{ Helper::loadAssetsFiles('build/assets/css/style.css') }}">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
