0b707ea37dc3ff11c27e0cbfdce39c480c71fc2d
vijay singh <vjmail17@gmail.com>
2018-01-20 06:43:13 +0530

    …

--- resources/views/front/pages/subject/index.blade.php
+++ resources/views/front/pages/subject/index.blade.php
@@ -37,49 +37,25 @@
 @stop
 
 @section('topiccontentheader')
-
-              <h1 class='headingtop'>{{ ! empty($topic_title) ? $topic_title : $info->topic}}</h1>
-               <div class="topnexprev">
-                 
-               <span class="share">
-                 <label>share with:</label>
-               
-                 <a class="btn btn-social-icon btn-facebook" title="share with facebook">
-                  <span class="fa fa-facebook"></span>
-                 </a>
-                 
-                 <a class="btn btn-social-icon btn-twitter" title="share with twitter">
-                  <span class="fa fa-twitter"></span>
-                 </a>
-                 
-                 <a class="btn btn-social-icon  btn-linkedin" title="share with twitter">
-                  <span class="fa fa-linkedin"></span>
-                 </a>
-                 
-                 <a class="btn btn-social-icon btn-google" title="share with google+">
-                  <span class="fa fa-google-plus"></span>
-                 </a>
-                 
-                 <a class="btn btn-social-icon btn-pinterest" title="share with pinterest">
-                  <span class="fa fa-pinterest"></span>
-                 </a>
-        
-               
-               </span>
-
-
-               @if (!empty($nextprevarr['prevlink']))
-                  <a  title="{{ $nextprevarr['prevtopic'] }}" href="{{ $nextprevarr['prevlink'] }}" class="btn btn-default"><i class="fa fa-arrow-left" aria-hidden="true"></i>  Previous</a>
-               @endif
-
-               @if (!empty($nextprevarr['nextlink']))
-                  <a title="{{ $nextprevarr['nexttopic'] }}" href="{{ $nextprevarr['nextlink'] }}" class="btn btn-default next">Next  <i class="fa fa-arrow-right" aria-hidden="true"></i> </a>
-               @endif
-
-
-               </div>
-
+        <h1 class='headingtop'>{{ ! empty($topic_title) ? $topic_title : $info->topic}}</h1>
+         <div class="topnexprev">
+          <div class="share">
+           <label>share with:</label>
+             <a class="btn btn-social-icon btn-facebook" title="share with facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+             <a class="btn btn-social-icon btn-twitter" title="share with twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+             <a class="btn btn-social-icon  btn-linkedin" title="share with twitter"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+             <a class="btn btn-social-icon btn-google" title="share with google+"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+             <a class="btn btn-social-icon btn-pinterest" title="share with pinterest"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+          </div>
+         @if (!empty($nextprevarr['prevlink']))
+            <a  title="{{ $nextprevarr['prevtopic'] }}" href="{{ $nextprevarr['prevlink'] }}" class="btn btn-default"><i class="fa fa-arrow-left" aria-hidden="true"></i>  Previous</a>
+         @endif
+         @if (!empty($nextprevarr['nextlink']))
+            <a title="{{ $nextprevarr['nexttopic'] }}" href="{{ $nextprevarr['nextlink'] }}" class="btn btn-default next">Next  <i class="fa fa-arrow-right" aria-hidden="true"></i> </a>
+         @endif
+       </div>
 @stop
+
 @section('content')
   {!!html_entity_decode($info->detail)!!}
 @stop
