c6b1ba0e462e39609c0131f633d51879828379d4
vijay singh <vjmail17@gmail.com>
2018-01-20 07:03:49 +0530

    ….

--- resources/views/front/layout/template.blade.php
+++ resources/views/front/layout/template.blade.php
@@ -57,7 +57,26 @@
              </div>
              <div class="col-md-8">
                <!-- navigation -->
-                 @yield('nav')
+                 <nav class="navbar navbar-inverse">
+                  <div class="container-fluid">
+                      <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navBar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span> 
+                        </button>
+                        <a class="navbar-brand" href="#"></a>
+                      </div>
+                      <div class="collapse navbar-collapse" id="navBar">
+                        <ul class="nav navbar-nav">
+                               <li class="active"><a href="#"><i class="fa fa-home" aria-hidden="true"></i></a></li>
+                               @foreach($subjects as $sub_data)
+                               <li><a style="" href="{{ route('topic', $sub_data->slug) }}">{{ ucfirst($sub_data->name) }}</a></li>
+                              @endforeach
+                        </ul>
+                      </div>
+                  </div>
+                </nav>
                <!-- close navigation -->
             </div>
            </div>
