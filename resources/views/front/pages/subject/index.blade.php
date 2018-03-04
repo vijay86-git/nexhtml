8b7f52f8a05339988cfcb7218bb45c3c90569119
vijay singh <vjmail17@gmail.com>
2018-03-04 10:52:06 +0530

    …

--- resources/views/front/pages/subject/index.blade.php
+++ resources/views/front/pages/subject/index.blade.php
@@ -19,11 +19,11 @@
          <div class="topnexprev">
           <div class="share">
            <label>share with:</label>
-             <a class="btn" title="share with facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-             <a class="btn" title="share with twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-             <a class="btn" title="share with twitter"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-             <a class="btn" title="share with google+"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-             <a class="btn" title="share with pinterest"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+             <a target="_blank" href="https://www.facebook.com/sharer.php?u={{ Request::url() }}" class="btn" title="share with facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+             <a target="_blank" href="http://twitter.com/share?url={{ Request::url() }}&text={{ $info->page_title }}" class="btn" title="share with twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+             <a target="_blank" href="https://www.facebook.com/sharer.php?u={{ Request::url() }}" class="btn" title="share with twitter"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+             <a target="_blank" href="https://plusone.google.com/_/+1/confirm?hl=en&url={{ Request::url() }}" class="btn" title="share with google+"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+             <a target="_blank" href="https://www.pinterest.com/pin/create/button/?url={{ Request::url() }}&description={{ $info->page_title }}" class="btn" title="share with pinterest"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
 
              @if (!empty($nextprevarr['prevlink']))
                 <a title="{{ $nextprevarr['prevtopic'] }}" href="{{ $nextprevarr['prevlink'] }}" class="btn btn-default"><i class="fa fa-arrow-left" aria-hidden="true"></i>  Previous</a>
