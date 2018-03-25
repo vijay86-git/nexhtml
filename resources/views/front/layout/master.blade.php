a69de408b19468bbd10e6d8d0b1d0cc2a4f7f9fe
vijay singh <vjmail17@gmail.com>
2018-03-25 14:14:59 +0530

    …

--- resources/views/front/layout/master.blade.php
+++ resources/views/front/layout/master.blade.php
@@ -7,12 +7,12 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Nexladder Online Web Tutorials</title>
     @php
-      echo $style_file_path = $_SERVER['DOCUMENT_ROOT'] . '/build/assets/css/style.css';
-      echo filemtime($style_file_path);
+      $style_file_path = $_SERVER['DOCUMENT_ROOT'] . '/build/assets/css/style.css';
+      $ftime           = filemtime($style_file_path);
     @endphp
     <link rel="stylesheet" href="{{ Helper::loadAssetsFiles('build/assets/css/bootstrap.min.css?v=1') }}">
     <link rel="stylesheet" href="{{ Helper::loadAssetsFiles('build/assets/css/font-awesome.css?v=1') }}">
-    <link rel="stylesheet" href="{{ Helper::loadAssetsFiles('build/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ Helper::loadAssetsFiles('build/assets/css/style.css') }}?t={{ $ftime }}">
     <style>
     .navbar-inverse .navbar-nav>li>a,.navbar-inverse .navbar-nav>li>a:hover  {color:#FFFFFF;font-size:14px;text-transform:uppercase}
     </style>
