d4565c5343a8441708da81833f56bdcd08a61b58
vijay singh <vjmail17@gmail.com>
2018-03-29 18:01:47 +0530

    ….

--- resources/views/front/pages/contact/index.blade.php
+++ resources/views/front/pages/contact/index.blade.php
@@ -37,7 +37,7 @@
     </div>
 
     <div class="col-md-1"></div>
-    <div class="col-md-5" style="width:480px;height:400px" id="map">
+    <div class="col-md-5" style="width:480px;height:350px" id="map">
     </div>
 
   </div>
