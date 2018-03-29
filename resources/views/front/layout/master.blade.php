5c8853ae815ea5722b07e74a439f1867cae6de4c
vijay singh <vjmail17@gmail.com>
2018-03-29 18:36:28 +0530

    …

--- resources/views/front/layout/master.blade.php
+++ resources/views/front/layout/master.blade.php
@@ -263,8 +263,8 @@
                 <p>&copy; 2018 All rights reserved.</p>
               </div>
 
-              <div class="col-md-6">
-                <a href="/">Contact Us</a>
+              <div class="col-md-6 pull-right">
+                <a href="/contact-us">Contact Us</a>
               </div>
               <!--/ copyright -->
             </div>
