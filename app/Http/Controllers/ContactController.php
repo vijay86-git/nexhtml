f5b8ca9ec1ed663ed63abece7d0fb797226d31e4
vijay singh <vjmail17@gmail.com>
2018-03-29 16:25:08 +0530

    ..

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -18,7 +18,7 @@ class ContactController extends Controller
   
 	    public function __construct()
 	      {
-	           $this->_subjects  =  Helper::getNavSubjectFromCache();
+	           //$this->_subjects  =  Helper::getNavSubjectFromCache();
 	      }
 
 	    public function index()
