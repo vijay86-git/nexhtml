3ee6f3c68faedfe9e76e70ad428e809b2e3aefb6
vijay singh <vjmail17@gmail.com>
2018-03-29 16:21:00 +0530

    …

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Subject as Subject;
+use App\Topic as Topics;
+use App\Section as Section;
 use DB;
 use Cache;
 use Helper;
