b4054844f17a446ecf67de1d9c7e4a807d1beb09
vijay singh <vjmail17@gmail.com>
2018-03-31 08:13:28 +0530

    ….

--- resources/views/front/pages/cms/disclaimer.blade.php
+++ resources/views/front/pages/cms/disclaimer.blade.php
@@ -4,11 +4,11 @@
  <div class="row">
     <div class="col-md-12" id="" style="height:400px">
         <h2>Disclaimer</h2>
-        <p><strong>POST Disclaimer</strong></p><br />
+        <p><strong>POST Disclaimer</strong></p>
         <p>All posts and information shared here are totally individual opinions and no any intention to harm or mislead any individual.</p>
 
         <p><strong>Emails USED</strong></p><br />
-        <p>All emails provided on the website or comment box are not shared with any third party websites or sold. We will never spam your inbox it’s our trust with you.</p><br /><br />
+        <p>All emails provided on the website or comment box are not shared with any third party websites or sold. We will never spam your inbox it’s our trust with you.</p><br />
 
         <p>for any concern or any issue related to privacy policies and disclaimer you can contact us at <a href="mailto:nexladdermail@gmail.com">nexladdermail@gmail.com</a></p>
        
