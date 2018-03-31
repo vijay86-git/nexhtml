f2fff8eb3901304115435b1fb7411c4e45bbdcef
vijay singh <vjmail17@gmail.com>
2018-03-31 08:16:34 +0530

    ….

--- resources/views/front/pages/cms/disclaimer.blade.php
+++ resources/views/front/pages/cms/disclaimer.blade.php
@@ -4,8 +4,8 @@
  <div class="row">
     <div class="col-md-12" id="" style="height:400px">
         <h2>Disclaimer</h2>
-        <p><strong>POST Disclaimer</strong></p>
-        <p>All posts and information shared here are totally individual opinions and no any intention to harm or mislead any individual.</p>
+        <p><strong>Topic Disclaimer</strong></p>
+        <p>All topics and information shared here are totally individual opinions and no any intention to harm or mislead any individual.</p>
 
         <p><strong>Emails Used</strong></p>
         <p>All emails provided on the website or contact us form are not shared with any third party websites or sold. We will never spam your inbox it’s our trust with you.</p><br />
