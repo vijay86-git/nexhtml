a413908175a4fc861372ebe7faf18f1731567145
vijay singh <vjmail17@gmail.com>
2018-03-31 08:04:02 +0530

    …

--- resources/views/front/pages/cms/about.blade.php
+++ resources/views/front/pages/cms/about.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
  <div class="row">
-    <div class="col-md-12" id="" style="height:500px">
+    <div class="col-md-12" id="" style="height:400px">
         <h2>About Us</h2>
         <p>
            About Us - nexladder.com is a learning online platform that helps anyone can learn Web Technologies. nexladder provides all web tutorials like Php, Mysqli, Vuejs, Html, Jquery etc in simple and easy steps starting from basic to advanced concepts with examples. nexladder helps students, developers and project manager where they can learn topics in easy and simple language with examples. To make our website better for you, we bring updates to the website regularly.
