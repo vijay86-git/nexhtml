e9eec9540ae56d2b686cd19f407c1e7c60af43dc
vijay singh <vjmail17@gmail.com>
2018-04-01 16:53:31 +0530

    …

--- resources/views/front/pages/cms/sitemap.blade.php
+++ resources/views/front/pages/cms/sitemap.blade.php
@@ -3,7 +3,7 @@
 .sitemap{float:left}
 .sitemap h2{font-size:16px}
 .sitemap ul {margin:0;padding:0}
-.sitemap ul li{padding:0 0 0 10px;line-height:16px}
+.sitemap ul li{padding:0 0 0 10px;line-height:16px;list-style-type:none}
 .sitemap ul li a{color:#888;font-size:13px}
 </style>
 
@@ -14,7 +14,7 @@
         <div class="col-md-3">
              <div class="sitemap">
                  <h2>php</h2>
-                 <ul class="points">
+                 <ul>
                     <li><a href="">Introduction</a></li>
                     <li><a href="">Overview</a></li>
                  </ul>
