387198f5ea3e789efa6faa2e023db7332fc29e8a
vijay singh <vjmail17@gmail.com>
2018-04-01 16:54:37 +0530

    …

--- resources/views/front/pages/cms/sitemap.blade.php
+++ resources/views/front/pages/cms/sitemap.blade.php
@@ -3,14 +3,14 @@
 .sitemap{float:left}
 .sitemap h2{font-size:16px}
 .sitemap ul {margin:0;padding:0}
-.sitemap ul li{padding:0 0 0 10px;line-height:16px;list-style-type:none}
-.sitemap ul li a{color:#888;font-size:13px}
+.sitemap ul li{padding:0 0 0 10px;line-height:20px;list-style-type:none}
+.sitemap ul li a{color:#505763;font-size:13px}
 </style>
 
 @section('content')
  <div class="row">
     <div class="col-md-12">
-        <h1>Sitemap</h1>
+        <a href="#"><h1>Sitemap</h1></a>
         <div class="col-md-3">
              <div class="sitemap">
                  <h2>php</h2>
