bcd6371a65c6e692a83048f86f9118208020aac5
vijay singh <vjmail17@gmail.com>
2018-04-01 18:18:36 +0530

    …

--- resources/views/front/pages/cms/sitemap.blade.php
+++ resources/views/front/pages/cms/sitemap.blade.php
@@ -1,11 +1,5 @@
 @extends('front.layout.template')
-<style>
-.sitemap{float:left;margin:0 0 15px 0;}
-.sitemap h2{font-size:16px;margin:0 0 7px 0;}
-.sitemap ul {margin:0;padding:0}
-.sitemap ul li{padding:0 0 0 10px;line-height:24px;list-style-type:none}
-.sitemap ul li a{color:#505763;font-size:13px}
-</style>
+
 
 @section('content')
  <div class="row">
