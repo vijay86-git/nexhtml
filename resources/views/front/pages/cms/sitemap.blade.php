312917131cfedac7d206d4005829d1b00b64fed0
vijay singh <vjmail17@gmail.com>
2018-04-01 16:12:29 +0530

    ….

--- resources/views/front/pages/cms/sitemap.blade.php
+++ resources/views/front/pages/cms/sitemap.blade.php
@@ -4,15 +4,13 @@
  <div class="row">
         <h2>Sitemap</h2>
         <div class="col-md-3">
-             <div class="content">
-                  <ul class="points">
-                     <li><a href="">PHP Tutorials</a></li>
-                     <ul class="points">
-                        <li><a href="">Introduction</a></li>
-                        <li><a href="">Overview</a></li>
-                     </ul>
-                  </ul>
-            </div>
+          <ul class="points">
+             <li><a href="">PHP Tutorials</a></li>
+             <ul class="points">
+                <li><a href="">Introduction</a></li>
+                <li><a href="">Overview</a></li>
+             </ul>
+          </ul>
         </div>
   </div>
 
