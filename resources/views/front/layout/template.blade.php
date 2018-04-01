74a085bcbb1dc022e784f619c976bba4cc8599d4
vijay singh <vjmail17@gmail.com>
2018-04-01 18:22:21 +0530

    ….

--- resources/views/front/layout/template.blade.php
+++ resources/views/front/layout/template.blade.php
@@ -186,7 +186,7 @@
               </div>
 
               <div class="col-md-6 pull-right" style="text-align:right">
-                <a title="Home" href="{{ route('home') }}">Home</a>&nbsp;|&nbsp;<a title="About" href="{{ route('about') }}">About</a>&nbsp;|&nbsp;<a title="Disclaimer" href="{{ route('disclaimer') }}">Disclaimer</a>&nbsp;|&nbsp;<a title="Contact Us" href="{{ route('contactus') }}">Contact Us</a>
+                <a title="Home" href="{{ route('home') }}">Home</a>&nbsp;|&nbsp;<a title="About" href="{{ route('about') }}">About</a>&nbsp;|&nbsp;<a title="Disclaimer" href="{{ route('disclaimer') }}">Disclaimer</a>&nbsp;|&nbsp;<a title="Contact Us" href="{{ route('contactus') }}">Contact Us</a>&nbsp;|&nbsp;<a title="Sitemap" href="{{ route('sitemap') }}">Sitemap</a>
               </div>
 
               <!--/ copyright -->
