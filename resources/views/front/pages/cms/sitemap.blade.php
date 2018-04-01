d9143697b4de74ab17d2b63d0731fd634fd1de6b
vijay singh <vjmail17@gmail.com>
2018-04-01 16:48:47 +0530

    …

--- resources/views/front/pages/cms/sitemap.blade.php
+++ resources/views/front/pages/cms/sitemap.blade.php
@@ -1,17 +1,23 @@
 @extends('front.layout.template')
+<style>
+.sitemap{float:left}
+.sitemap h2{font-size:16px}
+.sitemap ul {margin:0;padding:0}
+.sitemap ul li{padding:0 0 0 10px;line-height:16px}
+.sitemap ul li a{color:#888;font-size:13px}
+</style>
 
 @section('content')
  <div class="row">
-        <h2>Sitemap</h2>
+        <h1>Sitemap</h1>
         <div class="col-md-3">
-          <ul class="points">
-             <li><a href="">PHP Tutorials</a></li>
-             <ul class="points">
-                <li><a href="">Introduction</a></li>
-                <li><a href="">Overview</a></li>
-             </ul>
-          </ul>
+             <div class="sitemap">
+                 <h2>php</h2>
+                 <ul class="points">
+                    <li><a href="">Introduction</a></li>
+                    <li><a href="">Overview</a></li>
+                 </ul>
+            </div>
         </div>
   </div>
-
 @stop
