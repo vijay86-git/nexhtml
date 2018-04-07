7751c759b2807fe0dfb5514c447a5686ca48c27d
vijay singh <vjmail17@gmail.com>
2018-04-07 06:59:47 +0530

    ….

--- resources/views/front/pages/cms/about.blade.php
+++ resources/views/front/pages/cms/about.blade.php
@@ -2,6 +2,21 @@
 
 @section('content')
  <div class="row">
+    <div class="col-md-12">
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- Ad 1 -->
+        <ins class="adsbygoogle"
+             style="display:block"
+             data-ad-client="ca-pub-9716398444039739"
+             data-ad-slot="5095344703"
+             data-ad-format="auto"></ins>
+        <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
+ </div>
+
+ <div class="row">
     <div class="col-md-12" id="" style="height:400px">
         <h2>About Us</h2>
         <p>
