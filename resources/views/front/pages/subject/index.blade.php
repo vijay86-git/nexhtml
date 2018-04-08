4bdb89728b272c4adef80c501bb1a2d24d486245
vijay singh <vjmail17@gmail.com>
2018-04-08 11:49:23 +0530

    …

--- resources/views/front/pages/subject/index.blade.php
+++ resources/views/front/pages/subject/index.blade.php
@@ -14,6 +14,17 @@
                 </ul>
               @endforeach
            @endif
+
+           <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <!-- Ad 7 -->
+            <ins class="adsbygoogle"
+                 style="display:inline-block;width:300px;height:600px"
+                 data-ad-client="ca-pub-9716398444039739"
+                 data-ad-slot="5997201134"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+
    </div>
    <div class="col-md-9">
       <!-- content -->
