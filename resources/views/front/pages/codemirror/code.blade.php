9eaf5f58d0681e8fa4e075692eacab44c1c4252d
vijay singh <vjmail17@gmail.com>
2018-06-23 14:07:52 +0530

    …

--- resources/views/front/pages/codemirror/code.blade.php
+++ resources/views/front/pages/codemirror/code.blade.php
@@ -9,16 +9,17 @@
 	<meta property="og:image" content="/lib/images/signature.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link type="image/x-icon" href="/favicon.ico" rel="shortcut icon" />
-	<link rel="stylesheet" href="{{ URL::asset('codemirror/lib/bin/css/codemirror.css') }}" />
-	<link rel="stylesheet" href="{{ URL::asset('codemirror/lib/bin/css/html-mode-1.3.css') }}" />
-	<script type="text/javascript" src="{{ URL::asset('codemirror/lib/bin/js/jquery-1.7.js') }}"></script>
-	<script type="text/javascript" src="{{ URL::asset('codemirror/lib/bin/js/codemirror.js') }}"></script>
-	<script type="text/javascript" src="{{ URL::asset('codemirror/lib/bin/js/html-mode-1.1.js') }}"></script>
-	<script type="text/javascript" src="{{ URL::asset('codemirror/lib/bin/mode/xml.js') }}"></script>    
-	<script type="text/javascript" src="{{ URL::asset('codemirror/lib/bin/mode/javascript.js') }}"></script>
-	<script type="text/javascript" src="{{ URL::asset('codemirror/lib/bin/mode/css.js') }}"></script>
-	<script type="text/javascript" src="{{ URL::asset('codemirror/lib/bin/mode/htmlmixed.js') }}"></script>
-	<script type="text/javascript" src="{{ URL::asset('codemirror/lib/bin/js/active-line.js') }}"></script>
+    <meta name="msvalidate.01" content="F13063B8299624F18B7C4E98367AE3E1" />
+	<link rel="stylesheet" href="{{ URL::asset('codemirror/lib/bin/css/codemirror.css?v=1') }}" />
+	<link rel="stylesheet" href="{{ URL::asset('codemirror/lib/bin/css/html-mode-1.3.css?v=1') }}" />
+	<script type="text/javascript" src="{{ URL::asset('codemirror/lib/bin/js/jquery-1.7.js?v=1') }}"></script>
+	<script type="text/javascript" src="{{ URL::asset('codemirror/lib/bin/js/codemirror.js?v=1') }}"></script>
+	<script type="text/javascript" src="{{ URL::asset('codemirror/lib/bin/js/html-mode-1.1.js?v=1') }}"></script>
+	<script type="text/javascript" src="{{ URL::asset('codemirror/lib/bin/mode/xml.js?v=1') }}"></script>    
+	<script type="text/javascript" src="{{ URL::asset('codemirror/lib/bin/mode/javascript.js?v=1') }}"></script>
+	<script type="text/javascript" src="{{ URL::asset('codemirror/lib/bin/mode/css.js?v=1') }}"></script>
+	<script type="text/javascript" src="{{ URL::asset('codemirror/lib/bin/mode/htmlmixed.js?v=1') }}"></script>
+	<script type="text/javascript" src="{{ URL::asset('codemirror/lib/bin/js/active-line.js?v=1') }}"></script>
 	<script type="text/javascript">
 		$(document).ready(function(){
         	updatePreview();
@@ -29,6 +30,17 @@
 		.CodeMirror-activeline-background {background: #e8f2ff !important;}
 		.CodeMirror-scroll{overflow:auto;}
 	</style>
+
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-111558941-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-111558941-1');
+        </script>
 
 </head>
 <body>
