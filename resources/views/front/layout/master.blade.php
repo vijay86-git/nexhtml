b8dbdea2cd70d2fea1fbe44498face280293709f
vijay singh <vjmail17@gmail.com>
2018-07-03 08:07:39 +0530

    ..

--- resources/views/front/layout/master.blade.php
+++ resources/views/front/layout/master.blade.php
@@ -71,9 +71,9 @@
                 <div class="col-md-6">
                  <div class="social">
                   <ul>
-                  <li><a href="https://www.facebook.com/nexladder" title="facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                  <li><a href="https://twitter.com/nexladder" title="twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                  <li><a href="https://plus.google.com/b/107873437441310283848" title="google plus"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                  <li><a href="https://www.facebook.com/nexladder" title="facebook"><i class="fa fa-facebook homeclr" aria-hidden="true"></i></a></li>
+                  <li><a href="https://twitter.com/nexladder" title="twitter"><i class="fa fa-twitter homeclr" aria-hidden="true"></i></a></li>
+                  <li><a href="https://plus.google.com/b/107873437441310283848" title="google plus"><i class="fa fa-google-plus homeclr" aria-hidden="true"></i></a></li>
                 </ul>
                </div>
               </div>
