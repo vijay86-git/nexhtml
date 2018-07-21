a686562dd7f4472942fd3413ffe4df8610992ff0
vijay singh <vjmail17@gmail.com>
2018-07-21 18:15:16 +0530

    …

--- public/blog/wp-content/themes/llorix-one-lite/header.php
+++ public/blog/wp-content/themes/llorix-one-lite/header.php
@@ -14,6 +14,17 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php wp_head(); ?>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-111558941-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-111558941-1');
+    </script>
+        
 </head>
 
 <?php
