fed15724121175d6785ea686eb8013bcff434dbf
vijay singh <vjmail17@gmail.com>
2019-06-24 06:30:27 +0530

    …

--- resources/views/front/layout/template.blade.php
+++ resources/views/front/layout/template.blade.php
@@ -67,7 +67,6 @@
                   <ul>
                      <li><a href="https://www.facebook.com/nexladder" title="facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
                      <li><a href="https://twitter.com/nexladder" title="twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                     <li><a href="https://plus.google.com/b/107873437441310283848" title="google plus"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
                   </ul>
                </div>
               </div>
