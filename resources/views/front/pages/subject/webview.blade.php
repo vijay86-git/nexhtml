3f75ca66d0b09722a909adf2cff2f0840fb69dc7
vijay singh <vjmail17@gmail.com>
2019-09-04 06:26:27 +0530

    …

--- resources/views/front/pages/subject/webview.blade.php
+++ resources/views/front/pages/subject/webview.blade.php
@@ -3,9 +3,15 @@
 @section('content')
 
 <div class="row">
-   <div class="col-md-12" style="margin:15px 0 0 0">
+
+     <div class="col-md-12" style="margin:15px 0 0 0">
+        {!! $info->title !!}
+     </div>
+
+     <div class="col-md-12" style="margin:15px 0 0 0">
         {!! $info->detail !!}
      </div>
+
 </div>
 
 @stop
