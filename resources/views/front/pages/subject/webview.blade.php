f86433aa20c528a15d9d34a6ffa20bbdaa264c47
vijay singh <vjmail17@gmail.com>
2019-09-04 06:28:45 +0530

    …

--- resources/views/front/pages/subject/webview.blade.php
+++ resources/views/front/pages/subject/webview.blade.php
@@ -5,7 +5,7 @@
 <div class="row">
 
      <div class="col-md-12" style="margin:15px 0 0 0">
-        <h2>{!! $info->topic !!}</h2>
+        <h2><strong>{!! $info->topic !!}</strong></h2>
      </div>
 
      <div class="col-md-12" style="margin:15px 0 0 0">
