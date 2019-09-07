e84e74bdd0d4b0fa45099cf71122b6c2b721edfe
vijay singh <vjmail17@gmail.com>
2019-09-07 19:45:41 +0530

    …

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -107,12 +107,8 @@ class DashboardController extends Controller
 
     public function getBlogs()
      {
-         $blogs = DB::connection('blog')->table('tbl_posts')->select('ID', 'post_title')->where('post_type','post')->orderBy('ID', 'desc')->get()->toArray();
+         $blogs = DB::connection('blog')->table('tbl_posts')->select('ID', 'post_title')->orderBy('ID', 'desc')->get()->toArray();
          return response()->json(['response' => $blogs]);
      }
-
-
-
-
 
 }
