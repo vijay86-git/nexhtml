2d79102b4bfa8389427ace5eb9cb5e466893b5af
vijay singh <vjmail17@gmail.com>
2019-09-12 20:40:20 +0530

    …

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -130,7 +130,7 @@ class DashboardController extends Controller
 
     public function categoryBlogs($id = '')
      {
-         $query   = "SELECT * FROM tbl_posts LEFT JOIN tbl_term_relationships ON (tbl_posts.ID = tbl_term_relationships.object_id) LEFT JOIN tbl_term_taxonomy ON (tbl_term_relationships.term_taxonomy_id = tbl_term_taxonomy.term_taxonomy_id) WHERE tbl_term_taxonomy.term_id IN ($id) AND tbl_posts.post_status='publish'";
+         $query   = "SELECT tbl_posts.ID, tbl_posts.post_title FROM tbl_posts LEFT JOIN tbl_term_relationships ON (tbl_posts.ID = tbl_term_relationships.object_id) LEFT JOIN tbl_term_taxonomy ON (tbl_term_relationships.term_taxonomy_id = tbl_term_taxonomy.term_taxonomy_id) WHERE tbl_term_taxonomy.term_id IN ($id) AND tbl_posts.post_status='publish'";
          $results  = DB::connection('blog')->select($query);
 
          print_r($results);die;
