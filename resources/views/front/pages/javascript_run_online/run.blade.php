d765aa9e234f42fa4f772cf75a29c0c52537c847
vijay singh <vjmail17@gmail.com>
2019-12-24 22:52:17 +0530

    …

--- resources/views/front/pages/javascript_run_online/run.blade.php
+++ resources/views/front/pages/javascript_run_online/run.blade.php
@@ -21,13 +21,34 @@
 
  <div class="row">
     <div class="col-md-12" id="" style="height:400px">
-        <h2>About Us</h2>
-        <p>
-           About Us - nexladder.com is a learning online platform that helps anyone can learn Web Technologies. nexladder provides all web tutorials like Php, Mysqli, Vuejs, Html, Jquery etc in simple and easy steps starting from basic to advanced concepts with examples. nexladder helps students, developers and project manager where they can learn topics in easy and simple language with examples. To make our website better for you, we bring updates to the website regularly.
-        </p><br />
-        <p>
-           Our mission - To help you learn the skills you need to achieve your goal.
-        </p>
+        <h2>Run JavaScript Online Us</h2>
+
+        <section id="static">          
+            <pre>
+            <code id="static-js">console.log("Hello World!");
+// Hello World!
+            </code>
+            </pre>      
+            </section>      
+            <section id="live" class="live hidden">                    
+              <div id="editor" class="editor">
+              </div>          
+              <div class="output-container">              
+                <div class="buttons-container">                  
+                  <button id="execute" class="button run" type="button">Run &rsaquo;
+                  </button>                  
+                  <button id="reset" type="button" class="button">Reset
+                  </button>              
+                </div>              
+                <div id="console" class="output">
+                  <code>
+                  </code>
+                </div>          
+              </div>      
+            </section>    
+
+
+        
     </div>
 
     <div class="col-md-1"></div>
